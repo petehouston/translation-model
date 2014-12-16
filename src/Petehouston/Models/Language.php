12fb949f50e489cc976c95898556f04e9ea3b3e8
Pete Houston <contact@petehouston.com>
2014-12-16 22:11:52 +0700

    Init commit.

---
+++ src/Petehouston/Models/Language.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Database\Eloquent\SoftDeletingTrait;
+use Waavi\Translation\Models\Language as WaaviLanguage;
+
+class Language extends WaaviLanguage {
+    
+    use SoftDeletingTrait;
+    protected $dates = ['deleted_at'];
+}
